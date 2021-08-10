57a6219b14d6221b2fc93b6c8736440624f044f2
anonymous anonymous <wsun3@uchicago.edu>
2021-08-10 14:48:15 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,13 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :director_id, :integer
+  attribute :description, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
