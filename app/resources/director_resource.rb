0609ae23168285ff2a397623134eb896839ade8f
anonymous anonymous <wsun3@uchicago.edu>
2021-08-10 14:48:19 +0000

    Generate graphiti resources for directors

---
+++ app/resources/director_resource.rb
@@ -0,0 +1,13 @@
+class DirectorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :dob, :date
+  attribute :name, :string
+  attribute :bio, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
