df1484240a0e1fd7ea19650899a5b7379bcd99f9
anonymous anonymous <wsun3@uchicago.edu>
2021-08-10 14:46:43 +0000

    Direct association between actors and characters

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,10 @@
 class Actor < ApplicationRecord
   # Direct associations
 
+  has_many   :characters,
+             :foreign_key => "actors_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
