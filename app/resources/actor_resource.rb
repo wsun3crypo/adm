62836ae03517871b1fad1a3bcfa442b16190c0dc
anonymous anonymous <wsun3@uchicago.edu>
2021-08-10 14:48:24 +0000

    Generate graphiti resources for actors

---
+++ app/resources/actor_resource.rb
@@ -0,0 +1,11 @@
+class ActorResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
