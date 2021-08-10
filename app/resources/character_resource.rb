bf2fcb2a0557388167b74f6f8067a9b1e5ad3385
anonymous anonymous <wsun3@uchicago.edu>
2021-08-10 14:48:29 +0000

    Generate graphiti resources for characters

---
+++ app/resources/character_resource.rb
@@ -0,0 +1,12 @@
+class CharacterResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :movie_id, :integer
+  attribute :actors_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
