549b0d71f3ee94aaac772118f543d96f7e34a453
anonymous anonymous <wsun3@uchicago.edu>
2021-08-10 14:46:39 +0000

    Generate characters resource with ideas:scaffold

---
+++ app/views/characters/_character.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! character, :id, :movie_id, :actors_id, :created_at, :updated_at
+json.url character_url(character, format: :json)
---
+++ db/migrate/20210810144637_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :characters do |t|
+      t.integer :movie_id
+      t.integer :actors_id
+
+      t.timestamps
+    end
+  end
+end
